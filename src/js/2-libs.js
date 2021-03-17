256f189cd0c3dc97c148823b8fb495419dc5bf66
Scerka <pm@scerka.com>
2021-03-17 14:28:14 +0400

    Fix Yandex.Metrika

--- src/js/2-libs.js
+++ src/js/2-libs.js
@@ -138,4 +138,4 @@ tippy('[data-tippy-content]', {
     placement: 'bottom'
 });
 
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(72751489, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a.nextSibling)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(72751489, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });
